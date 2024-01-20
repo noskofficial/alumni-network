1e823d1dc77b43425994f6b5df3d3f6e7886438a
Mukesh Kumar Chaudhary <mukezhz@gmail.com>
2024-01-20 23:10:16 +0545

    fix: make responsive

--- frontend/components/TheHero.vue
+++ frontend/components/TheHero.vue
@@ -8,19 +8,19 @@
   </div>
 
   <div class="bg-[#045F89]">
-    <div class="w-[60%] mx-auto">
-      <div class="flex justify-center gap-x-24">
+    <div class="w-[90%] md:w-[80%] lg:w-[70%] xl:w-[60%] mx-auto">
+      <div class="flex justify-center md:gap-x-2 flex-wrap md:flex-nowrap">
         <div class="flex flex-col">
-          <h3 class="text-2xl text-white mt-5">Alumni Nipesh Shrestha </h3>
-          <h2 class="text-xl text-white mt-1">on NCIT 25th Anniversary</h2>
-          <p class="text-white mt-3 w-[90%]">Nipesh Talked about his life at NCIT, what he learnt
+          <h3 class="text-3xl text-white mt-5">Alumni Nipesh Shrestha </h3>
+          <h2 class="text-2xl text-white mt-2">on NCIT 25th Anniversary</h2>
+          <p class="text-white mt-4 w-[90%]">Nipesh Talked about his life at NCIT, what he learnt
             what he did, how life was, how many pushups he did in college and how he managed Gym and study at the same
             time.
             .. </p>
-          <button class=" border-solid bg-red-600 w-1/2 text-white rounded-md mt-16 p-3 text-sm">Full Story</button>
+          <button class=" border-solid bg-red-600 w-1/2 text-white rounded-md mt-16 p-3 text-sm mb-5">Full Story</button>
         </div>
         <div class="w-[100%] h-80 bg-cover">
-          <img src="~/assets/png/ncit.png" alt="" class="w-[600px] h-[88%] m-5">
+          <img src="~/assets/png/ncit.png" alt="" class="w-[600px] h-[88%] my-5">
         </div>
       </div>
     </div>
