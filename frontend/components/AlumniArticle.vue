92946c54c1315235b15f2c9a193424296d851607
Mukesh Kumar Chaudhary <mukezhz@gmail.com>
2024-01-21 16:00:58 +0545

    fix: add support for dark mode in all size screen

--- frontend/components/AlumniArticle.vue
+++ frontend/components/AlumniArticle.vue
@@ -1,13 +1,13 @@
 <template>
-  <div :class="bgColor">
+  <div :class="bgColor" class="text-black dark:text-white">
     <div class="flex flex-col md:flex-row w-[95%] md:w-[90%] lg:w-[85%] xl:w-[65%] mx-auto">
       <div class="flex justify-center flex-wrap md:flex-nowrap" :class="direction">
         <div class="flex flex-col">
-          <h3 class="text-3xl text-white mt-5">{{ title }}
-            <h2 class="text-2xl text-white mt-2">{{ titleEvent }}</h2>
+          <h3 class="text-3xl mt-5">{{ title }}
+            <h2 class="text-2xl mt-2">{{ titleEvent }}</h2>
           </h3>
-          <p class="text-white mt-4 w-[90%]">{{ description }}</p>
-          <button class=" border-solid bg-red-600 w-1/2 text-white rounded-md mt-16 p-3 text-sm mb-5">Full
+          <p class="mt-4 w-[90%]">{{ description }}</p>
+          <button class=" border-solid bg-red-600 w-1/2 rounded-md mt-16 p-3 text-sm mb-5">Full
             Story</button>
         </div>
         <div class="mx-0 md:mx-5 xl:mx-8"></div>
--- frontend/layouts/landing.vue
+++ frontend/layouts/landing.vue
@@ -1,16 +1,18 @@
 
 import { StoryProgramSection } from '#build/components';
 <template>
-    <BannerNav />
-    <TheNav />
-    <TheHero />
-    <section class="bg-gray-50">
-        <StoryProgramSection />
-    </section>
-    <section>
-        <AboutAlumni/>
-    </section>
-    <footer class="bg-[#081E2C] text-gray-300">
-        <TheFooter />
-    </footer>
+    <div class="text-black dark:text-white">
+        <BannerNav />
+        <TheNav />
+        <TheHero />
+        <section class="bg-gray-50 dark:bg-gray-800">
+            <StoryProgramSection />
+        </section>
+        <section>
+            <AboutAlumni />
+        </section>
+        <footer class="bg-[#081E2C] text-gray-300">
+            <TheFooter />
+        </footer>
+    </div>
 </template>
