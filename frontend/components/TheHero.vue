567621ff6effaaf87d1044d9978b92664bdf1204
Mukesh Kumar Chaudhary <mukezhz@gmail.com>
2024-01-21 16:13:08 +0545

    feat: make hero image blur

--- frontend/components/TheHero.vue
+++ frontend/components/TheHero.vue
@@ -1,12 +1,22 @@
 <template>
   <div>
-    <div class="bg-[url('/assets/png/ncit_college.png')]">
-      <div class="flex flex-col justify-center items-center h-[496px]">
-        <img src="~/assets/png/ncit.png" alt="NCIT Logo" class="w-40">
-        <h1 class="text-3xl sm:text-5xl text-white mt-10">NCIT Alumni Network</h1>
-        <h2 class="text-2xl sm:text-4xl text-white mt-6">All Alumni in One Place</h2>
+    <div class="relative">
+      <div class="bg-[url('/assets/png/ncit_college.png')] blur-sm">
+        <div class="flex flex-col justify-center items-center h-[496px]">
+          <img src="~/assets/png/ncit.png" alt="NCIT Logo" class="w-40">
+          <h1 class="text-3xl sm:text-5xl mt-10">NCIT Alumni Network</h1>
+          <h2 class="text-2xl sm:text-4xl mt-6">All Alumni in One Place</h2>
+        </div>
+      </div>
+      <div class="absolute top-0 left-[50%] -translate-x-[50%]">
+        <div class="flex flex-col justify-center items-center h-[496px]">
+          <img src="~/assets/png/ncit.png" alt="NCIT Logo" class="w-40">
+          <h1 class="text-3xl sm:text-5xl mt-10">NCIT Alumni Network</h1>
+          <h2 class="text-2xl sm:text-4xl mt-6">All Alumni in One Place</h2>
+        </div>
       </div>
     </div>
+
 
     <AlumniArticle title="Alumni Nipesh Shrestha" title-event="on NCIT 25th Anniversary"
       description="Nipesh Talked about his life at NCIT, what he learnt what he did, how life was, how many pushups he did in college and how he managed Gym and study at the same time... "
